72100b278000fcd9c01372fcc61f39c1dec4c9bc
CoreySumma <CoreySumma@gmail.com>
2023-03-31 23:44:48 -0500

    fixed logo aspect ratio

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,14 @@
 import logo from "../../assets/two.png";
+import "./LandingPage.css";
 
 export default function LandingPage () {
   return (
-    <header>
-    <h1 className="text-5xl">Skip the appetizer and cut right to the main course.</h1>
+    <div>
+    <h1 className="text-4xl">Skip the appetizer and cut right to the main course.</h1>
     <br />
-    <p className="text-3xl">Get the run down on all your daily emails with the click of a button.</p>
+    <p className="text-2xl">Get the run down on all your daily emails with the click of a button.</p>
     <img src={logo} className="App-logo" alt="logo" />
   <p>(Powered by OpenAI)</p>
-  </header>
+  </div>
   )
 }
